ae16cfe4dc3a32a3ffeae94ffd8637b9c19d38a3
Marcio Sardou <marciosut1996@gmail.com>
2024-09-15 18:59:08 -0300

    cleanup: removendo arquivos desnecessarios e organizando para refatoração

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-// import { Container } from './styles';
-
-const app: React.FC = () => {
-  return <div>FLUMINENSE</div>
-}
-
-export default app
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,8 @@
-import type { Metadata } from 'next'
-import { Inter, Roboto } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google'
 import ThemeRegistry from '@theme/ThemeRegistry'
+import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Pomodoro Clock',
-  description:
-    'A simple Pomodoro Timer app that works on a desktop & mobile browser. Pomofucus will help you manage your time and let you focus on any tasks such as study, writing, or coding.'
-}
 
 export default function RootLayout({
   children
